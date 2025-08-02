655915340bd2594890d53617deea0f6f0e6ea737
kovalchuk14 <kovalcukanton682@gmail.com>
2025-08-02 15:27:25 +0200

    deploy fix

--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -7,13 +7,13 @@ import { fetchNotes } from "@/lib/api";
 import NotesClient from "./Notes.client";
 
 type Props = {
- params: { slug: string[] };
+  params: Promise<{ slug: string[] }>;
   searchParams?: { q?: string; page?: string };
 };
 
 async function App({ params, searchParams }: Props) {
   const { slug } = await params;
-  const category = params.slug[0] === 'All' ? undefined : params.slug[0];
+  const category = slug[0] === 'All' ? undefined : slug[0];
   const searchQuery = searchParams?.q ?? "";
   const currentPage = Number(searchParams?.page) || 1;
 
